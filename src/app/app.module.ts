ab540b0a9be2ec1aea3b804e40b25ba7aab55bf1
Flori <florentina-maria.ciordas@my.fmi.unibuc.ro>
2023-04-25 22:42:14 +0300

    Revert "fix gitHub pages routing"
    This reverts commit 950e1c34c04921300948f4086925d47e063dac0d.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,3 @@
-import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
@@ -6,18 +5,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouteReuseStrategy } from '@angular/router';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule, BrowserAnimationsModule],
-  providers: [
-    {provide: RouteReuseStrategy, useClass: IonicRouteStrategy},
-    {provide: LocationStrategy, useClass: HashLocationStrategy}
-  ],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
-export class AppModule {
-}
+export class AppModule {}
